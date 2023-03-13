0adc27f198bf616d131bed5ac3b8753bace77f4c
mehwish <afreenmehwish2@gmail.com>
2023-03-13 09:24:13 +0530

    done with backend

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const app = express();
 const route = require('./src/route')
-
+const cors = require('cors')
 app.use(express.json());
 
 app.use('/', route)
-
+app.use(cors())
 app.listen(3000, () => {
   console.log('Server started on port 3000');
 });
